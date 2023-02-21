75f0c8a77228ab3ae22d53f0893374ef2a7bd742
Victor Ramirez <vicdeveloper83@gmail.com>
2023-02-21 19:19:16 +0000

    orders route completed

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -24,4 +24,105 @@ const newOrder = async (req, res, next) => {
 		});
 	}
 };
-module.exports = { newOrder };
+//? get single order api/v1/order/:id
+const getSingleOrder = async (req, res, next) => {
+	try {
+		const order = await Order.findById(req.params.id);
+		if (!order) {
+			throw new Error('No se encontro ninguna orden con ese ID');
+		}
+		res.status(200).send({
+			success: true,
+			order,
+		});
+	} catch (error) {
+		res.status(404).send({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+//?get logged in user order /api/v1/orders/me
+const myOrders = async (req, res, next) => {
+	try {
+		const orders = await Order.find({ user: req.user.id });
+
+		res.status(200).send({
+			success: true,
+			orders,
+		});
+	} catch (error) {
+		res.status(404).send({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+//?get all orders /api/v1/admin/orders
+const allOrders = async (req, res, next) => {
+	try {
+		const orders = await Order.find();
+
+		let totalAmount = 0;
+
+		orders.forEach((order) => {
+			totalAmount += order.itemsPrice;
+		});
+		res.status(200).send({ success: true, totalAmount, orders });
+	} catch (error) {
+		res.status(400).send({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+//?update order ADMIN /api/v1/admin/order/:id
+const updateOrder = async (req, res, next) => {
+	const { status } = req.body;
+	try {
+		const order = await Order.findById(req.params.id);
+		if (!order) {
+			throw new Error('La order con ese ID no existe');
+		}
+		order.orderStatus = status;
+		order.deliveredAt = Date.now();
+		order.save();
+		res.status(200).send({
+			success: true,
+			order,
+		});
+	} catch (error) {
+		res.status(404).send({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+//?delete order admin /api/v1/admin/order/delete/:id
+const deleteOrder = async (req, res, next) => {
+	try {
+		const order = await Order.findByIdAndDelete(req.params.id);
+		if (!order) {
+			throw new Error('La order con ese ID no existe');
+		}
+
+		order.save();
+		res.status(200).send({
+			success: true,
+		});
+	} catch (error) {
+		res.status(404).send({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+module.exports = {
+	newOrder,
+	getSingleOrder,
+	myOrders,
+	allOrders,
+	updateOrder,
+	updateOrder,
+	deleteOrder,
+};
--- api/routes/order.js
+++ api/routes/order.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder } = require('../controllers/order.controller.js');
+const {
+	newOrder,
+	getSingleOrder,
+	myOrders,
+	allOrders,
+	updateOrder,
+	deleteOrder,
+} = require('../controllers/order.controller.js');
 const { isAuthenticated, authorizeRoles } = require('../middlewares/auth.js');
 
 router.post('/order/new', isAuthenticated, newOrder);
+router.get('/order/:id', isAuthenticated, getSingleOrder);
+router.get('/orders/me', isAuthenticated, myOrders);
+router.get(
+	'/admin/orders',
+	isAuthenticated,
+	authorizeRoles('admin'),
+	allOrders
+);
+router.put(
+	'/admin/order/:id',
+	isAuthenticated,
+	authorizeRoles('admin'),
+	updateOrder
+);
+router.delete(
+	'/admin/order/:id',
+	isAuthenticated,
+	authorizeRoles('admin'),
+	deleteOrder
+);
 
 module.exports = router;
